23795feae4c3ba12d3d7a00733f32f3b9e411430
Jonas Felix de Souza <jonaspetty@hotmail.com>
2022-08-04 22:59:49 -0300

    making some changes to the requests functions

--- scripts/projects_request.js
+++ scripts/projects_request.js
@@ -1,24 +1,46 @@
 // Requests the public projects on my GitHub
-const requestURL = "https://api.github.com/users/Jonas-petty/repos"
+window.onload = function() {
+    var pageName = getPageName();
+    var projectsJson = requestProjects(pageName);
+}
 
-let request = new XMLHttpRequest()
-
-request.open('GET', requestURL)
-request.responseType = 'json'
-request.send()
-
-request.onload = function() {
-    var projectsResponse = request.response
-    showSomeProjects(projectsResponse)
+function getPageName() {
+    var path = window.location.pathname
+    var page = path.substring(path.lastIndexOf('/') + 1)
+    return page
 }
 
 
-function showSomeProjects(jsonObject) {
+function requestProjects(PageName) {
+    const requestURL = "https://api.github.com/users/Jonas-petty/repos"
+
+    let request = new XMLHttpRequest()
+
+    request.open('GET', requestURL)
+    request.responseType = 'json'
+    request.send()
+
+    request.onload = function() {
+        var projectsResponse = request.response
+        console.log(projectsResponse, PageName)
+        if (PageName == 'index.html') {
+            showProjects(projectsResponse, 6)
+        } else if (PageName == 'projects.html') {
+            //showProjects(projectsResponse)
+        }
+    }
+}
+
+function showProjects(jsonObject, nProjects) {
     var projects = jsonObject
 
     var projectsGrid = document.getElementById('projects_grid')
+
+    if (nProjects == null) {
+        nProjects = projects.length
+    }
     
-    for (var i = 0; i <= 6; i++) {
+    for (var i = 0; i <= nProjects; i++) {
         if (projects[i]['name'] != 'Jonas-petty') {
             var myA = document.createElement('a')
             myA.href = projects[i]['html_url']
@@ -48,3 +70,21 @@ function showSomeProjects(jsonObject) {
         }
     }
 }
+
+/*
+function showAllProjects(jsonObject) {
+    var projects = jsonObject
+
+    var area = document.getElementById('all_projects')
+
+    for (var i = 0; i <= projects.length; i++) {
+        var myP = document.createElement("p")
+        var myBr = document.createElement("br")
+        myP.textContent =`${projects[i]['name']}`
+        console.log(projects[i]['name'])
+
+        area.appendChild(myP)
+        area.appendChild(myBr)
+    }
+}
+*/
